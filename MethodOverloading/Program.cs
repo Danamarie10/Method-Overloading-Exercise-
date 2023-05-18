95272c39638e823258a0ed36186563a5495f7038
Danamarie10 <dana.patrick10@outlook.com>
2023-05-18 12:20:52 -0400

    finished exercise

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -1,10 +1,46 @@
-﻿namespace MethodOverloading
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace MethodOverloading
 {
     public class Program
     {
+        public static int Add(int num1, int num2)
+        {
+           return num1 + num2;
+            
+        }
+        public static double Add(double num1, double num2)
+        {
+            return num1 + num2;
+            
+        }
+        public static void Add(int money1, int money2, bool isTrue)
+        {
+            
+            var moneyAnswer = money1 + money2; 
+
+            if (moneyAnswer >= 1 && isTrue)
+            {
+                Console.WriteLine(moneyAnswer + " dollars");
+            }
+             else if (moneyAnswer == 1 && isTrue)
+         
+            {
+                Console.WriteLine(moneyAnswer + " dollar");
+            }
+            else
+            {
+                Console.WriteLine(moneyAnswer);
+            }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+           var firstAnswer =  Add(2, 2);
+            Console.WriteLine(firstAnswer);
+           var secondAnswer = Add(3.3, 3.3);
+            Console.WriteLine(secondAnswer);
+             Add(11, 13, true);
+                
         }
     }
 }
